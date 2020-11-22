140f1b8a5aca4aef8794e149568e8c4c5a74fe80
Felix Fontein <felix@fontein.de>
2020-11-22 21:30:46 +0100

    Add docker connection tests.

--- tests/integration/targets/connection_docker/runme.sh
+++ tests/integration/targets/connection_docker/runme.sh
@@ -1,1 +1,42 @@
-../connection_posix/test.sh
+#!/usr/bin/env bash
+
+# Setup phase
+
+echo "Setup"
+ANSIBLE_ROLES_PATH=.. ansible-playbook setup.yml
+
+# If docker wasn't installed, don't run the tests
+if [ "$(command -v docker)" == "" ]; then
+    exit
+fi
+
+
+# Test phase
+
+
+DOCKER_CONTAINERS="docker-connection-test-container"
+
+[[ -n "$DEBUG" || -n "$ANSIBLE_DEBUG" ]] && set -x
+
+set -euo pipefail
+
+cleanup() {
+    echo "Cleanup"
+    docker rm -f ${DOCKER_CONTAINERS}
+    echo "Shutdown"
+    ANSIBLE_ROLES_PATH=.. ansible-playbook shutdown.yml
+    echo "Done"
+    exit 0
+}
+
+trap cleanup INT TERM EXIT
+
+echo "Start containers"
+for CONTAINER in ${DOCKER_CONTAINERS}; do
+    docker run --rm --name ${CONTAINER} --detach python:3-alpine /bin/sh -c 'sleep 10m'
+    docker exec ${CONTAINER} pip3 install coverage
+    echo ${CONTAINER}
+done
+
+echo "Run tests"
+./runme-connection.sh
