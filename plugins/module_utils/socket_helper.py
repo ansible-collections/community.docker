c6867098686950bbaad385094595f96581544370
Felix Fontein <felix@fontein.de>
2021-04-05 16:34:29 +0200

    Fix copyright lines.

--- plugins/module_utils/socket_helper.py
+++ plugins/module_utils/socket_helper.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, Felix Fontein <felix@fontein.de>
+# Copyright (c) 2019-2021, Felix Fontein <felix@fontein.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
--- plugins/modules/docker_container_exec.py
+++ plugins/modules/docker_container_exec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2016 Red Hat | Ansible
+# Copyright (c) 2021, Felix Fontein <felix@fontein.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
