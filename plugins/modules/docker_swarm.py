a22633cf737299bc8cdaf0b8a08c19914c602a84
Ville Ojamo <14869000+bluikko@users.noreply.github.com>
2023-04-05 19:12:27 +0700

    docker_swarm: improve wording

--- plugins/modules/docker_swarm.py
+++ plugins/modules/docker_swarm.py
@@ -80,7 +80,8 @@ options:
       - Set to C(join), to join an existing cluster.
       - Set to C(absent), to leave an existing cluster.
       - Set to C(remove), to remove an absent node from the cluster.
-        Note that removing requires Docker SDK for Python >= 2.4.0. Managers need to be demoted with the module M(community.docker.docker_node) before removal.
+      - M(community.docker.docker_node) can be used to demote a manager before removal.
+      Note that removing requires Docker SDK for Python >= 2.4.0.
     type: str
     default: present
     choices:
