a3377be9986c38f5c22fc34ba34ab270aef3c7b1
Felix Fontein <felix@fontein.de>
2024-01-17 07:06:19 +0100

    Fix examples and return values for docker_compose_v2_pull.

--- plugins/modules/docker_compose_v2_pull.py
+++ plugins/modules/docker_compose_v2_pull.py
@@ -52,212 +52,12 @@ seealso:
 '''
 
 EXAMPLES = '''
-# Examples use the django example at https://docs.docker.com/compose/django. Follow it to create the
-# flask directory
-
-- name: Run using a project directory
-  hosts: localhost
-  gather_facts: false
-  tasks:
-    - name: Tear down existing services
-      community.docker.docker_compose_v2:
-        project_src: flask
-        state: absent
-
-    - name: Create and start services
-      community.docker.docker_compose_v2:
-        project_src: flask
-      register: output
-
-    - name: Show results
-      ansible.builtin.debug:
-        var: output
-
-    - name: Run `docker-compose up` again
-      community.docker.docker_compose_v2:
-        project_src: flask
-      register: output
-
-    - name: Show results
-      ansible.builtin.debug:
-        var: output
-
-    - ansible.builtin.assert:
-        that: not output.changed
-
-    - name: Stop all services
-      community.docker.docker_compose_v2:
-        project_src: flask
-        state: stopped
-      register: output
-
-    - name: Show results
-      ansible.builtin.debug:
-        var: output
-
-    - name: Verify that web and db services are not running
-      ansible.builtin.assert:
-        that:
-          - "not output.services.web.flask_web_1.state.running"
-          - "not output.services.db.flask_db_1.state.running"
-
-    - name: Restart services
-      community.docker.docker_compose_v2:
-        project_src: flask
-        state: restarted
-      register: output
-
-    - name: Show results
-      ansible.builtin.debug:
-        var: output
-
-    - name: Verify that web and db services are running
-      ansible.builtin.assert:
-        that:
-          - "output.services.web.flask_web_1.state.running"
-          - "output.services.db.flask_db_1.state.running"
+- name: Pull images for flask project
+  community.docker.docker_compose_v2_pull:
+    project_src: /path/to/flask
 '''
 
 RETURN = '''
-containers:
-  description:
-    - A list of containers associated to the service.
-  returned: success
-  type: list
-  elements: dict
-  contains:
-    Command:
-      description:
-        - The container's command.
-      type: raw
-    CreatedAt:
-      description:
-        - The timestamp when the container was created.
-      type: str
-      sample: "2024-01-02 12:20:41 +0100 CET"
-    ExitCode:
-      description:
-        - The container's exit code.
-      type: int
-    Health:
-      description:
-        - The container's health check.
-      type: raw
-    ID:
-      description:
-        - The container's ID.
-      type: str
-      sample: "44a7d607219a60b7db0a4817fb3205dce46e91df2cb4b78a6100b6e27b0d3135"
-    Image:
-      description:
-        - The container's image.
-      type: str
-    Labels:
-      description:
-        - Labels for this container.
-      type: dict
-    LocalVolumes:
-      description:
-        - The local volumes count.
-      type: str
-    Mounts:
-      description:
-        - Mounts.
-      type: str
-    Name:
-      description:
-        - The container's primary name.
-      type: str
-    Names:
-      description:
-        - List of names of the container.
-      type: list
-      elements: str
-    Networks:
-      description:
-        - List of networks attached to this container.
-      type: list
-      elements: str
-    Ports:
-      description:
-        - List of port assignments as a string.
-      type: str
-    Publishers:
-      description:
-        - List of port assigments.
-      type: list
-      elements: dict
-      contains:
-        URL:
-          description:
-            - Interface the port is bound to.
-          type: str
-        TargetPort:
-          description:
-            - The container's port the published port maps to.
-          type: int
-        PublishedPort:
-          description:
-            - The port that is published.
-          type: int
-        Protocol:
-          description:
-            - The protocol.
-          type: str
-          choices:
-            - tcp
-            - udp
-    RunningFor:
-      description:
-        - Amount of time the container runs.
-      type: str
-    Service:
-      description:
-        - The name of the service.
-      type: str
-    Size:
-      description:
-        - The container's size.
-      type: str
-      sample: "0B"
-    State:
-      description:
-        - The container's state.
-      type: str
-      sample: running
-    Status:
-      description:
-        - The container's status.
-      type: str
-      sample: Up About a minute
-images:
-  description:
-    - A list of images associated to the service.
-  returned: success
-  type: list
-  elements: dict
-  contains:
-    ID:
-      description:
-        - The image's ID.
-      type: str
-      sample: sha256:c8bccc0af9571ec0d006a43acb5a8d08c4ce42b6cc7194dd6eb167976f501ef1
-    ContainerName:
-      description:
-        - Name of the conainer this image is used by.
-      type: str
-    Repository:
-      description:
-        - The repository where this image belongs to.
-      type: str
-    Tag:
-      description:
-        - The tag of the image.
-      type: str
-    Size:
-      description:
-        - The image's size in bytes.
-      type: int
 actions:
   description:
     - A list of actions that have been applied.
@@ -271,12 +71,8 @@ actions:
       type: str
       sample: container
       choices:
-        - container
         - image
-        - network
-        - service
         - unknown
-        - volume
     id:
       description:
         - The ID of the resource that was changed.
@@ -286,16 +82,8 @@ actions:
       description:
         - The status change that happened.
       type: str
-      sample: Creating
+      sample: Pulling
       choices:
-        - Starting
-        - Exiting
-        - Restarting
-        - Creating
-        - Stopping
-        - Killing
-        - Removing
-        - Recreating
         - Pulling
 '''
 
