14e2f92974c52968f50a9b6132d94cf6f65b9e00
Felix Fontein <felix@fontein.de>
2024-03-13 13:56:22 +0100

    Improve inventory integration tests. (#817)

--- tests/integration/targets/inventory_docker_swarm/runme.sh
+++ tests/integration/targets/inventory_docker_swarm/runme.sh
@@ -9,17 +9,17 @@ set -euo pipefail
 
 cleanup() {
     echo "Cleanup"
-    ansible-playbook playbooks/swarm_cleanup.yml
+    ansible-playbook playbooks/swarm_cleanup.yml "$@"
     echo "Done"
 }
 
 trap cleanup INT TERM EXIT
 
 echo "Setup"
-ANSIBLE_ROLES_PATH=.. ansible-playbook  playbooks/swarm_setup.yml
+ANSIBLE_ROLES_PATH=.. ansible-playbook  playbooks/swarm_setup.yml "$@"
 
 echo "Test docker_swarm inventory 1"
-ansible-playbook -i inventory_1.docker_swarm.yml playbooks/test_inventory_1.yml
+ansible-playbook -i inventory_1.docker_swarm.yml playbooks/test_inventory_1.yml "$@"
 
 echo "Test docker_swarm inventory 2"
-ansible-playbook -i inventory_2.docker_swarm.yml playbooks/test_inventory_2.yml
+ansible-playbook -i inventory_2.docker_swarm.yml playbooks/test_inventory_2.yml "$@"
