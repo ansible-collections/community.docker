ab8b6662c28a2c7b0473d43fcf0b26adb2877ede
Felix Fontein <felix@fontein.de>
2024-05-20 21:08:25 +0200

    Add hotfix for requests 2.32.0. (#861)

--- plugins/module_utils/_api/transport/basehttpadapter.py
+++ plugins/module_utils/_api/transport/basehttpadapter.py
@@ -18,3 +18,10 @@ class BaseHTTPAdapter(_HTTPAdapter):
         super(BaseHTTPAdapter, self).close()
         if hasattr(self, 'pools'):
             self.pools.clear()
+
+    # Hotfix for requests 2.32.0: its commit
+    # https://github.com/psf/requests/commit/c0813a2d910ea6b4f8438b91d315b8d181302356
+    # changes requests.adapters.HTTPAdapter to no longer call get_connection() from
+    # send(), but instead call _get_connection().
+    def _get_connection(self, request, *args, **kwargs):
+        return self.get_connection(request.url, kwargs.get('proxies'))
