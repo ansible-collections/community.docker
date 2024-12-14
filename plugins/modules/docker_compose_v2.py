882755eff1416aa69eecd1a2719038a33420ffec
Felix Fontein <felix@fontein.de>
2024-12-14 18:39:38 +0100

    Switch default to true.

--- plugins/modules/docker_compose_v2.py
+++ plugins/modules/docker_compose_v2.py
@@ -88,9 +88,9 @@ options:
       - If O(state=present) and O(ignore_build_events=true) and O(build=always), a rebuild that does
         not trigger a container restart no longer results in RV(ignore:changed=true).
       - Note that Docker Compose 2.31.0 is the first Compose 2.x version to emit build events.
-        For older versions, the behavior is always as if O(ignore_build_events=false).
+        For older versions, the behavior is always as if O(ignore_build_events=true).
     type: bool
-    default: false
+    default: true
     version_added: 4.2.0
   recreate:
     description:
@@ -650,7 +650,7 @@ def main():
         scale=dict(type='dict'),
         wait=dict(type='bool', default=False),
         wait_timeout=dict(type='int'),
-        ignore_build_events=dict(type='bool', default=False),
+        ignore_build_events=dict(type='bool', default=True),
     )
     argspec_ex = common_compose_argspec_ex()
     argument_spec.update(argspec_ex.pop('argspec'))
