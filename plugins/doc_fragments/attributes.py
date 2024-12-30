436818489ebe0d45e8171b3fcd7e5aa8b9a7318e
Felix Fontein <felix@fontein.de>
2024-12-30 20:06:53 +0100

    Mention check mode in attribute description.
    Co-authored-by: Alexei Znamensky <103110+russoz@users.noreply.github.com>

--- plugins/doc_fragments/attributes.py
+++ plugins/doc_fragments/attributes.py
@@ -20,7 +20,7 @@ attributes:
     description: Will return details on what has changed (or possibly needs changing in C(check_mode)), when in diff mode.
   idempotent:
     description:
-      - When run twice in a row with the same arguments, the second invocation indicates no change.
+      - When run twice in a row outside check mode, with the same arguments, the second invocation indicates no change.
       - This assumes that the system controlled/queried by the module has not changed in a relevant way.
 """
 
