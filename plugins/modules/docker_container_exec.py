226240ce233b0d9a1c2d253362ebb041d32b1f39
Felix Fontein <felix@fontein.de>
2025-01-25 19:11:31 +0100

    Also mention glob patterns and redirects.

--- plugins/modules/docker_container_exec.py
+++ plugins/modules/docker_container_exec.py
@@ -101,7 +101,8 @@ notes:
     closing the connection with Python's C(SSLSocket)s. See U(https://github.com/ansible-collections/community.docker/issues/605)
     for more information.
   - If you need to evaluate environment variables of the container in O(command) or O(argv), you need to pass the command
-    through a shell, like O(command=/bin/sh -c "echo $ENV_VARIABLE").
+    through a shell, like O(command=/bin/sh -c "echo $ENV_VARIABLE"). The same needs to be done in case you want to use glob patterns
+    or other shell features such as redirects.
 author:
   - "Felix Fontein (@felixfontein)"
 
