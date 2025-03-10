9e0843eed484de9763045708d7286757bc803d31
Jonas Geiler <git@jonasgeiler.com>
2025-03-10 21:48:44 +0100

    refactor(docker_compose_v2): use `-y` instead of `--y` to ensure future compatibility
    Maybe they'll change it back to `--yes` sometime, so I'll use the short form that most likely won't change.

--- plugins/modules/docker_compose_v2.py
+++ plugins/modules/docker_compose_v2.py
@@ -162,7 +162,7 @@ options:
     version_added: 3.8.0
   assume_yes:
     description:
-      - When O(assume_yes=true), pass C(--y) to assume "yes" as answer to all prompts and run non-interactively.
+      - When O(assume_yes=true), pass C(-y) to assume "yes" as answer to all prompts and run non-interactively.
       - Right now a prompt is asked whenever a non-matching volume should be re-created. O(assume_yes=false)
         results in the question being answered by "no", which will simply re-use the existing volume.
       - This option is only available on Docker Compose 2.32.0 or newer.
@@ -537,7 +537,7 @@ class ServicesManager(BaseComposeManager):
         if dry_run:
             args.append('--dry-run')
         if self.yes:
-            args.append('--y')
+            args.append('-y')
         args.append('--')
         for service in self.services:
             args.append(service)
