d50dedd4cdc5372d89cc8fcf2052e61cab1a219d
Felix Fontein <felix@fontein.de>
2025-04-19 19:53:23 +0200

    [stable-3] Run extra sanity tests with nox (#1069)
    * Run extra sanity tests with nox. (#1068)
    
    (cherry picked from commit e20118b68f813fc00ef1f8e254cfcd06460f0af3)
    
    * Fix CI.
    
    * Adjust ignore file.

---
+++ noxfile.py
@@ -0,0 +1,27 @@
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 Felix Fontein <felix@fontein.de>
+
+# /// script
+# dependencies = ["nox>=2025.02.09", "antsibull-nox"]
+# ///
+
+import sys
+
+import nox
+
+
+try:
+    import antsibull_nox
+except ImportError:
+    print("You need to install antsibull-nox in the same Python environment as nox.")
+    sys.exit(1)
+
+
+antsibull_nox.load_antsibull_nox_toml()
+
+
+# Allow to run the noxfile with `python noxfile.py`, `pipx run noxfile.py`, or similar.
+# Requires nox >= 2025.02.09
+if __name__ == "__main__":
+    nox.main()
