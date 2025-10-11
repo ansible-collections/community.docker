8acdc3c85178ea481338447b27ac3e79ee1c4b04
Felix Fontein <felix@fontein.de>
2025-10-11 14:29:35 +0200

    Address consider-using-dict-comprehension.

--- plugins/module_utils/_util.py
+++ plugins/module_utils/_util.py
@@ -56,13 +56,11 @@ DOCKER_COMMON_ARGS = dict(
     debug=dict(type="bool", default=False),
 )
 
-DOCKER_COMMON_ARGS_VARS = dict(
-    [
-        [option_name, f"ansible_docker_{option_name}"]
-        for option_name in DOCKER_COMMON_ARGS
-        if option_name != "debug"
-    ]
-)
+DOCKER_COMMON_ARGS_VARS = {
+    option_name: f"ansible_docker_{option_name}"
+    for option_name in DOCKER_COMMON_ARGS
+    if option_name != "debug"
+}
 
 DOCKER_MUTUALLY_EXCLUSIVE = []
 
