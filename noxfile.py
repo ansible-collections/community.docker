c5831a79f7baaa6d94cb8fe0301d442d672ab8e7
Felix Fontein <felix@fontein.de>
2026-06-01 07:03:13 +0200

    Use nox in AZP CI. (#1267)

--- noxfile.py
+++ noxfile.py
@@ -12,12 +12,31 @@ import nox
 
 try:
     import antsibull_nox
+    from antsibull_nox.cli import run as run_antsibull_nox
 except ImportError:
     print("You need to install antsibull-nox in the same Python environment as nox.")
     sys.exit(1)
 
 
 antsibull_nox.load_antsibull_nox_toml()
+
+
+@nox.session(name="update-azp-config", python=False)
+def update_azp_config(session: nox.Session) -> None:
+    command = [
+        "antsibull-nox",
+        "update-azp-config",
+        "--exclude-tags",
+        "docker",
+        "--min-ansible-core",
+        "2.18",
+    ]
+    if antsibull_nox.IN_CI:
+        command.append("--fail-on-change")
+    session.debug(" ".join(command))
+    result = run_antsibull_nox(command)
+    if result != 0:
+        session.fail(f"Execution failed with status code {result}")
 
 
 # Allow to run the noxfile with `python noxfile.py`, `pipx run noxfile.py`, or similar.
