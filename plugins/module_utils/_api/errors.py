d9854205c8d0e2c503f73e04f77eac58f1c428c7
Felix Fontein <felix@fontein.de>
2026-09-14 22:37:33 +0200

    Update list of reasons for not found images. (#1315)
    (cherry picked from commit d7208094b60fb3be11800b33f0c7d5e06ae21a3e)

--- plugins/module_utils/_api/errors.py
+++ plugins/module_utils/_api/errors.py
@@ -39,7 +39,10 @@ def create_api_error_from_http_exception(e):
         if explanation and ('No such image' in str(explanation) or
                             'not found: does not exist or no pull access'
                             in str(explanation) or
-                            'repository does not exist' in str(explanation)):
+                            'repository does not exist' in str(explanation) or
+                            'was found but does not match the specified platform'
+                            in str(explanation) or
+                            'failed to resolve reference' in str(explanation)):
             cls = ImageNotFound
         else:
             cls = NotFound
